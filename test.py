2bd0f761aad087aa17137cf7a82af2297daa5418
EliorFoy <1700118540@qq.com>
2025-01-08 14:30:11 +0800

    更新readme头

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ session = BUPT.login_with_verify()
 
 
 BUPT.grab_all_course(session)
-# a = BUPT.get_chosen_courses(session)
-# print(a)
+a = BUPT.get_chosen_courses(session)
+print(a)
 # BUPT.unchoose_course(session,"电路综合设计")
